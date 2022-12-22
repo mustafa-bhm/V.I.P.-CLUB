cc44319c75258dc6517dd809f96d0b654b421e54
mustafa <mustafa.bhm3@gmail.com>
2022-12-22 11:50:39 -0700

    allow users to create blogs and save to mongodb

--- app.js
+++ app.js
@@ -27,21 +27,148 @@ const User = mongoose.model(
     password: { type: String, required: true },
   })
 );
-
+const Post = mongoose.model(
+  "Post",
+  new Schema(
+    {
+      author: { type: String, required: true },
+      post: { type: String, required: true },
+    },
+    { timestamps: true }
+  )
+);
 /// **** Middleware *** ////
+app.use(express.static("public"));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            // if password matches
+            return done(null, user);
+          } else {
+            // if password do not match
+            return done(null, false, { message: "Incorect password" });
+          }
+        })
+      ) {
+      }
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+app.use(passport.initialize());
+app.use(passport.session());
+// to add user object Locals object under currentUser , so it can be available to all the views
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+app.use(express.urlencoded({ extended: false }));
 
 //// ** Routes *** ////
+
+//=> home page
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/dashboard", (req, res) => {
+  if (req.user) {
+    res.render("dashboard");
+  } else {
+    res.send("<h3> you need to log-in/register to see this page</h3>");
+  }
+});
+
+//=> login page
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
+//=> registration page
 app.get("/register", (req, res) => {
   res.render("register");
 });
+//=> create new user
+app.post("/sign-up", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      user
+        .save()
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+});
+
+//=> to login users
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+//=> to logout users
+
+app.get("/log-out", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+//=> to create new post
+app.post("/new-post", (req, res) => {
+  const post = new Post(req.body);
+  post
+    .save()
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // console.log("pooost", post);
+});
+
 app.listen(3003, () => {
   console.log(`server is running on port ${port}`);
 });
